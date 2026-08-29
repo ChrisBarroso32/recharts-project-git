0f29bcf9a9ab63a7d853ffc80d896c416740c71b
Christian <chrisbarroso32@gmail.com>
2026-08-28 21:01:34 -0600

    Modificación inicial de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>App</div>
+        <main className="flex min-h-screen flex-col items-center justify-center px-4 md:px-8 xl:px-10 py-10">
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 w-full gap-10 max-w-[1400px]">
+            <div className="h-[400px] w-full bg-slate-800/50 p-4 rounded-xl border border-slate-800">
+
+            </div>
+
+            <div className="h-[400px] w-full bg-slate-800/50 p-4 rounded-xl border border-slate-800">
+
+            </div>
+
+            <div className="h-[400px] w-full bg-slate-800/50 p-4 rounded-xl border border-slate-800 xl:col-span-1 lg:col-span-2">
+
+            </div>
+          </div>
+        </main>
     )
 }
 export default App
